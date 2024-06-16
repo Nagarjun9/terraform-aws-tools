bee7fe525533f97bd4b90296dd678025697272e1
nagarjun9 <nagunaggu@gmail.com>
2024-06-16 19:41:12 +0530

    cicd

--- cicd/main.tf
+++ cicd/main.tf
@@ -29,36 +29,35 @@ module "jenkins_agent" {
   }
 }
 
-resource "aws_key_pair" "tools" {
-  key_name   = "tools"
-  # you can paste the public key directly like this
-  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
-  public_key = file("~/.ssh/tools.pub")
-  # ~ means windows home directory
-}
+# resource "aws_key_pair" "tools" {
+#   key_name   = "tools"
+#   # you can paste the public key directly like this
+#   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
+#   public_key = file("~/.ssh/tools.pub")
+#   # ~ means windows home directory
+# }
 
+# module "nexus" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
 
-module "nexus" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+#   name = "nexus"
 
-  name = "nexus"
-
-  instance_type          = "t3.medium"
-  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
-  # convert StringList to list and get first element
-  subnet_id = "subnet-0ea509ad4cba242d7"
-  ami = data.aws_ami.nexus_ami_info.id
-  key_name = aws_key_pair.tools.key_name
-  root_block_device = [
-    {
-      volume_type = "gp3"
-      volume_size = 30
-    }
-  ]
-  tags = {
-    Name = "nexus"
-  }
-}
+#   instance_type          = "t3.medium"
+#   vpc_security_group_ids = ["sg-04227d08605d6ac31"]
+#   # convert StringList to list and get first element
+#   subnet_id = "subnet-0fc3b0fcae5c65801"
+#   ami = data.aws_ami.nexus_ami_info.id
+#   key_name = aws_key_pair.tools.key_name
+#   root_block_device = [
+#     {
+#       volume_type = "gp3"
+#       volume_size = 30
+#     }
+#   ]
+#   tags = {
+#     Name = "nexus"
+#   }
+# }
 
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
@@ -74,6 +73,7 @@ module "records" {
       records = [
         module.jenkins.public_ip
       ]
+      allow_overwrite = true
     },
     {
       name    = "jenkins-agent"
@@ -82,16 +82,118 @@ module "records" {
       records = [
         module.jenkins_agent.private_ip
       ]
-    },
-    {
-      name    = "nexus"
-      type    = "A"
-      ttl     = 1
       allow_overwrite = true
-      records = [
-        module.nexus.private_ip
-      ]
     }
+    # ,
+    # {
+    #   name    = "nexus"
+    #   type    = "A"
+    #   ttl     = 1
+    #   allow_overwrite = true
+    #   records = [
+    #     module.nexus.private_ip
+    #   ]
+    #   allow_overwrite = true
+    # }
   ]
 
 }
+
+
+# module "jenkins" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+
+#   name = "jenkins-tf"
+
+#   instance_type          = "t3.small"
+#   vpc_security_group_ids = ["sg-04227d08605d6ac31"] #replace your SG
+#   subnet_id = "subnet-0fc3b0fcae5c65801" #replace your Subnet
+#   ami = data.aws_ami.ami_info.id
+#   user_data = file("jenkins.sh")
+#   tags = {
+#     Name = "jenkins-tf"
+#   }
+# }
+
+# module "jenkins_agent" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+
+#   name = "jenkins-agent"
+
+#   instance_type          = "t3.small"
+#   vpc_security_group_ids = ["sg-04227d08605d6ac31"]
+#   # convert StringList to list and get first element
+#   subnet_id = "subnet-0fc3b0fcae5c65801"
+#   ami = data.aws_ami.ami_info.id
+#   user_data = file("jenkins-agent.sh")
+#   tags = {
+#     Name = "jenkins-agent"
+#   }
+# }
+
+# resource "aws_key_pair" "tools" {
+#   key_name   = "tools"
+#   # you can paste the public key directly like this
+#   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
+#   public_key = file("~/.ssh/tools.pub")
+#   # ~ means windows home directory
+# }
+
+
+# module "nexus" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+
+#   name = "nexus"
+
+#   instance_type          = "t3.medium"
+#   vpc_security_group_ids = ["sg-04227d08605d6ac31"]
+#   # convert StringList to list and get first element
+#   subnet_id = "subnet-00fc3b0fcae5c65801"
+#   ami = data.aws_ami.nexus_ami_info.id
+#   key_name = aws_key_pair.tools.key_name
+#   root_block_device = [
+#     {
+#       volume_type = "gp3"
+#       volume_size = 30
+#     }
+#   ]
+#   tags = {
+#     Name = "nexus"
+#   }
+# }
+
+# module "records" {
+#   source  = "terraform-aws-modules/route53/aws//modules/records"
+#   version = "~> 2.0"
+
+#   zone_name = var.zone_name
+
+#   records = [
+#     {
+#       name    = "jenkins"
+#       type    = "A"
+#       ttl     = 1
+#       records = [
+#         module.jenkins.public_ip
+#       ]
+#     },
+#     {
+#       name    = "jenkins-agent"
+#       type    = "A"
+#       ttl     = 1
+#       records = [
+#         module.jenkins_agent.private_ip
+#       ]
+#     },
+#     {
+#       name    = "nexus"
+#       type    = "A"
+#       ttl     = 1
+#       allow_overwrite = true
+#       records = [
+#         module.nexus.private_ip
+#       ]
+#     }
+#   ]
+
+# }
